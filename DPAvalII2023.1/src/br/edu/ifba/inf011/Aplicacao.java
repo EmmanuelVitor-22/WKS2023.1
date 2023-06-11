df42ff1085bbdba88a87b4f2b1b03a5d78d95120
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-06-10 22:05:07 -0300

    teste

--- DPAvalII2023.1/src/br/edu/ifba/inf011/Aplicacao.java
+++ DPAvalII2023.1/src/br/edu/ifba/inf011/Aplicacao.java
@@ -7,46 +7,37 @@ import br.edu.ifba.inf011.model.MusicaNotas;
 import br.edu.ifba.inf011.model.Player;
 import br.edu.ifba.inf011.model.resources.ResourceLoader;
 
-public class Aplicacao{
+public class Aplicacao {
 
-	
 	private void musica() throws IOException {
 
 		ResourceLoader.DIR_NAME = "D:\\workspace\\DPAvalII2023.1\\src\\br\\edu\\ifba\\inf011\\model\\resources\\data\\";
-		
-		Musica musica =  new MusicaNotas("GodSaveTheQueen");
+
+		Musica musica = new MusicaNotas("GodSaveTheQueen");
 		System.out.println(musica.execute());
-		
-		
+
 	}
-	
-	
+
 	private void teste() throws IOException {
 
 		ResourceLoader.DIR_NAME = "D:\\workspace\\DPAvalII2023.1\\src\\br\\edu\\ifba\\inf011\\model\\resources\\data\\";
 		Player player = new Player();
 		int iCount = 0;
 
-		//......
-		
-		while(player.temProximo() && iCount < 30) {
+		// ......
+
+		while (player.temProximo() && iCount < 30) {
 			System.out.println(player.proximo());
 			iCount++;
 		}
-		
+
 	}
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		Aplicacao app = new Aplicacao();
 		app.musica();
 		app.teste();
+
 	}
-
-
-
-
-
-
-	
 
 }
